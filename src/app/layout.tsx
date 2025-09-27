a2fc9652b35b5e960d6b4a83d1f218be971a053b
Aadi <63931334+Aadi-1@users.noreply.github.com>
2025-09-27 13:26:48 -0700

    added msft clarity to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import ClarityScript from "./components/clarityscript";
 
 const dancingScript = Dancing_Script({
   subsets: ["latin"],
@@ -36,6 +37,7 @@ export default function RootLayout({
             content="Bark n' Roll Pet Care offers professional dog walking, pet sitting, and overnight services to keep your furry friends happy and healthy."
           />
         </Head>
+        <ClarityScript />
         {/* Navbar */}
 
         {/* Render Page Content */}
